a2d6ec333828bfac1b99f5c041d3406ea41d24af
akash-12398 <napinderbajwa@gmail.com>
2020-09-23 15:25:36 -0500

    first commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+            for (int x = 1; x < 300; x++)
+
+                if (x % 5 == 0 && x % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (x % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (x % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+        }
+    }
+}
